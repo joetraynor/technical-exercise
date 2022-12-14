7cda5424576e8423a4924d02be49440dc87f2d2c
Joe Traynor <joetraynor9@hotmail.com>
2022-12-14 19:58:40 +0000

    add nodemon. return only required commit fields: name, date and message

--- packages/api/index.js
+++ packages/api/index.js
@@ -1,8 +1,24 @@
-const express = require('express')
+// const express = require('express')
+import express from 'express'
+import fetch from 'node-fetch';
+import 'dotenv'
 
 const server = express()
   .get('/api', (req, res) => {
-    res.json({ data: { message: 'hey there' } })
+    fetch('https://api.github.com/repositories/44838949/commits?per_page=100&sha=f45309246584ebdbc0cd6f4960c3f2103ff76a76')
+      .then(res => res.json())
+      .then(response => {
+        const newArray = response.map(obj => {
+          const commit = obj.commit
+          return {
+            name: commit.author.name,
+            date: commit.author.date,
+            message: commit.message
+          }
+        })
+        res.send(newArray)
+      })
+      .catch(err => console.error('error:' + err))
   })
   .listen('3001', () => {
     console.log('api listening', server.address())
