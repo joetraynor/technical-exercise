0c41cd0f381bec613b0aa75ebf4f1df01877c331
Joe Traynor <joetraynor9@hotmail.com>
2022-12-15 09:20:26 +0000

    remove loading from commit component as it is no longer needed

--- packages/app/src/Commit.js
+++ packages/app/src/Commit.js
@@ -1,12 +1,10 @@
 import './App.css';
 
 const Commit = (props) => {
-    return props.isLoading === true ? (
-<p>loading</p>
-    ) : (
-    <div className='Commit'>
-        <p className='MessageText'>{props.message}</p>
-        <p className='MessageDetails'>{props.name}, {props.date}</p>
-    </div>
+    return (
+        <div className='Commit'>
+            <p className='MessageText'>{props.message}</p>
+            <p className='MessageDetails'>{props.name}, {props.date}</p>
+        </div>
     )}
 export default Commit;
